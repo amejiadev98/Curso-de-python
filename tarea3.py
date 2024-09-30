aad658a8d1a46bd88144470cc321ef7a9c14daf6
amejiadev98 <102830950+amejiadev98@users.noreply.github.com>
2024-09-30 14:56:57 -0400

    Create tarea3.py

---
+++ tarea3.py
@@ -0,0 +1,13 @@
+#1.	Diseñar un algoritmo que lea 4 variables y calcule e imprima su producto y suma
+A = int(input("Ingrese el primer valor"))
+B = int(input("Ingrese el segundo valor"))
+C = int(input("Ingrese el tercer valor"))
+D = int(input("Ingrese el cuarto valor"))
+
+Suma = A + B + C + D
+Producto = A * B * C * D
+
+
+print(f"la suma de los productos es {Suma}")
+print(f"El total valor de los producto es {Producto}")
+ 
