d08a952640478dd5c2a8627f991fc9c639c5f8c8
amejiadev98 <102830950+amejiadev98@users.noreply.github.com>
2024-09-30 14:59:40 -0400

    Create tarea4.py

---
+++ tarea4.py
@@ -0,0 +1,11 @@
+#2.	Diseñar un algoritmo que lea 5 variables y calcule e imprima su promedio
+a = float(input("Promedio del primer periodo "))
+b = float(input("Promedio del segundo periodo "))
+c = float(input("Promedio del tercer periodo "))
+d = float(input("Promedio del cuarto periodo "))
+e = float(input("Promedio del quinto periodo "))
+
+promedio = (a + b + c + d + e) /5
+
+print(f"el promedio total de los periodos universitario es {promedio}")
+
